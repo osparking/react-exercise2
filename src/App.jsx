1c523b52826a98263c5f1975a03eb6f8c906c1c0
박종범 <jbpark03@gmail.com>
2025-05-11 08:27:35 +0900

    provide initVal, add reset button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import useCounter from './useCounter';
 
 function App() {
-  const {count, incCount, decCount} = useCounter();
+  const {count, incCount, decCount, reset} = useCounter(100);
   const counter = useCounter();
 
   return (
@@ -11,9 +11,11 @@ function App() {
         <h1>계수: {count}</h1>
         <button onClick={incCount}>증가</button>
         <button onClick={decCount}>감소</button>
+        <button onClick={reset}>초기화</button>
         <h1>계수: {counter.count}</h1>
         <button onClick={counter.incCount}>증가</button>
         <button onClick={counter.decCount}>감소</button>
+        <button onClick={counter.reset}>초기화</button>
       </div>
     </>
   )
