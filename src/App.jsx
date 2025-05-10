2da8f5908af2d4e1a33ec71ef3ed825068e880e0
박종범 <jbpark03@gmail.com>
2025-05-10 20:16:52 +0900

    counter state and inc/dec functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const incCount = () => setCount(count + 1);
+  const decCount = () => setCount(count - 1);
 
   return (
     <>
       <div>
-        <h1>맞춤 후크</h1>
+        <h1>계수: {count}</h1>
       </div>
     </>
   )
