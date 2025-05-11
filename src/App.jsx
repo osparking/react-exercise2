9a9a2c866fdce781e2397fea2c232ac8155cb943
박종범 <jbpark03@gmail.com>
2025-05-11 09:55:59 +0900

    calls fetch on App loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }, []);
+  
   return (
     <>
       <div>
