88c2ea16725d43ec7e40e4efa4a4c870100c730c
박종범 <jbpark03@gmail.com>
2025-05-11 12:32:00 +0900

    handle error reading posts

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [ data, setData ] = useState([]);
+  const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     setIsLoading(true)
@@ -11,19 +12,29 @@ function App() {
       .then(response => response.json())
       .then(json => {
         setData(json);
-        setIsLoading(false)});
+        setIsLoading(false)
+      })
+      .catch(error => {
+        console.log("에러: ", error);
+        setError(error);
+        isLoading(false);        
+      });
   }, []);
 
   if (isLoading) {
     return <p>자료 적재 중...</p>
   }
-  
+
+  if (error) {
+    return <p>자료 적재 오류 발생</p>
+  }
+
   return (
     <>
       <div>
         <h1>API 사용하기</h1>
         <ul>
-          {data.map((post) => 
+          {data.map((post) =>
             <li key={post.id}>
               <p><strong>제목: {post.title}</strong></p>
               <p>내용: {post.body}</p>
