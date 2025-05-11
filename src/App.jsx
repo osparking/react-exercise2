e268d7fb61bab34aee61744e031e4641375f9de8
박종범 <jbpark03@gmail.com>
2025-05-11 18:35:38 +0900

    function skeleton, add note form tag

--- src/App.jsx
+++ src/App.jsx
@@ -3,48 +3,17 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const handleSubmit = (e) => {
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios.all([
-      axios.get('https://jsonplaceholder.typicode.com/posts'),
-      axios.get('https://jsonplaceholder.typicode.com/users')]
-    ).then(axios.spread((posts, users) => {
-      console.log(posts);
-      console.log(users);
-      setData(posts.data);
-      setIsLoading(false);
-      // throw new Error("포스트 자료 적재 오류 발생!");
-    })
-    ).catch(error => {
-        console.error("에러: ", error);
-        setError(error);
-        setIsLoading(false);
-      });
-  }, []);
-
-  if (isLoading) {
-    return <p>자료 적재 중...</p>
-  }
-
-  if (error) {
-    return <p>자료 적재 오류 발생</p>
   }
 
   return (
     <>
       <div>
         <h1>API 사용하기</h1>
-        <ul>
-          {data.map((post) =>
-            <li key={post.id}>
-              <p><strong>제목: {post.title}</strong></p>
-              <p>내용: {post.body}</p>
-            </li>)}
-        </ul>
+  <form onSubmit={handleSubmit}>
+    <button type="submit">노트 추가</button>
+  </form>
       </div>
     </>
   )
