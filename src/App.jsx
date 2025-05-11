2787ef58ef2ab6163d0c1b0d07ebb030b9396860
박종범 <jbpark03@gmail.com>
2025-05-11 09:07:11 +0900

    init state to practice making API calls

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
 import './App.css';
-import useCounter from './useCounter';
 
 function App() {
-  const {count, incCount, decCount, reset} = useCounter(100);
-  const counter = useCounter();
-
   return (
     <>
       <div>
-        <h1>계수: {count}</h1>
-        <button onClick={incCount}>증가</button>
-        <button onClick={decCount}>감소</button>
-        <button onClick={reset}>초기화</button>
-        <h1>계수: {counter.count}</h1>
-        <button onClick={counter.incCount}>증가</button>
-        <button onClick={counter.decCount}>감소</button>
-        <button onClick={counter.reset}>초기화</button>
+        <h1>API 사용하기</h1>
       </div>
     </>
   )
