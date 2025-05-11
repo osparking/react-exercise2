19745c3ac9974a62d855ce13cc0c62dac18b9c41
박종범 <jbpark03@gmail.com>
2025-05-11 19:58:13 +0900

    make handleSubmit complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,29 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
   const handleSubmit = (e) => {
-
+    const newPost = {
+      title: 'foo',
+      body: 'bar',
+      userId: 1,
+    };
+    axios.post(
+      "https://jsonplaceholder.typicode.com/posts", 
+      newPost)
+    .then(response => {
+      console.log("추가된 노트: ", response.data);
+      setData([response.data]);
+    })
   }
-
+  
   return (
     <>
       <div>
         <h1>API 사용하기</h1>
-  <form onSubmit={handleSubmit}>
-    <button type="submit">노트 추가</button>
-  </form>
+        <form onSubmit={handleSubmit}>
+          <button type="submit">노트 추가</button>
+        </form>
       </div>
     </>
   )
