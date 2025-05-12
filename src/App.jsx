3a8f311bda0cc40a08aafb154004cba28f92de97
박종범 <jbpark03@gmail.com>
2025-05-12 21:35:52 +0900

    insert error message below name input

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [errors, setErrors] = useState({});
   const validateForm = () => {
     if (formData.name.trim().length === 0) {
-      setErrors({...errors, name: '이름이 누락되었습니다!'});
+      setErrors({ ...errors, name: '이름이 누락되었습니다!' });
     }
   }
 
@@ -19,14 +19,15 @@ function App() {
     validateForm();
     if (Object.keys(errors).length === 0) {
       console.log("제출 폼 내용: ", formData);
-    }   
+    }
   }
 
   const handleChange = (e) => {
     console.log("name: ", e.target.value);
     setFormData({
-      ...formData, 
-      [e.target.name]: e.target.value});
+      ...formData,
+      [e.target.name]: e.target.value
+    });
   }
 
   return (
@@ -35,16 +36,20 @@ function App() {
       <h1>리액트 폼</h1>
       <form onSubmit={handleSubmit}>
         <label>이름:
-          <input 
-          type='text' name='name' 
-          value={formData.name}
-          onChange={handleChange}/>
+          <input
+            type='text' name='name'
+            value={formData.name}
+            onChange={handleChange} />
+          {errors.name &&
+            <><br /><span style={{ color: 'red' }}>
+              {errors.name}</span></>}
         </label>
+        <br />
         <label>이메일:
-          <input 
-          type='email' name='email' 
-          value={formData.email}
-          onChange={handleChange}/>
+          <input
+            type='email' name='email'
+            value={formData.email}
+            onChange={handleChange} />
         </label>
         <button type="submit">폼 제출</button>
       </form>
