e0dbf08e1048dc15348e064faee6bc3d8a01bfd1
박종범 <jbpark03@gmail.com>
2025-05-12 15:58:57 +0900

    formData state, submit handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import './App.css';
 
 function App() {
+  const [formData, setFormData] = useState({
+    name: '(성명)'
+  })
+
+  const handleSubmit = () => {
+    console.log("제출 폼 내용: ", formData);    
+  }
 
   return (
+    
     <div>
       <h1>리액트 폼</h1>
     </div>
