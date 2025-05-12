5841ef27e4f1d28e7fc5e172db0dd34ffe150120
박종범 <jbpark03@gmail.com>
2025-05-12 10:26:10 +0900

    put Authori' header to req' inter'

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,19 @@
 import axios from "axios";
 
 const axi = axios.create({
-  baseURL : "https://jsonplaceholder.typicode.com",
-  headers : {
-    "Authorization" : "Bearer <TOKEN>"
+  baseURL: "https://jsonplaceholder.typicode.com",
+  headers: {
+    'Content-Type': 'application/json'
   }
 });
 
 axi.interceptors.request.use((request) => {
   console.log("request: ", request);
-  return request;  
+  const token = localStorage.getItem("token");
+  if (token) {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+  return request;
 });
 
 export default axi;
