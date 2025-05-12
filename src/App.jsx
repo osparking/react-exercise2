9b34d0db70daee32fa83bc82e6ddd9ceb0205394
박종범 <jbpark03@gmail.com>
2025-05-13 08:32:37 +0900

    remove 2 inputs inside label tags

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,9 @@ function App() {
       <h1>리액트 폼</h1>
       <form onSubmit={handleSubmit}>
         <label>이름:
-          <input
-            type='text' name='name'
-            value={formData.name}
-            onChange={handleChange} />
-          {errors.name &&
-            <><br /><span style={{ color: 'red' }}>
-              {errors.name}</span></>}
         </label>
         <br />
         <label>이메일:
-          <input
-            type='email' name='email'
-            value={formData.email}
-            onChange={handleChange} />
         </label>
         <button type="submit">폼 제출</button>
       </form>
